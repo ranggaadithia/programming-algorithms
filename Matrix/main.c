6164105083660947eb628a100a09e29f4ca6ebba
Rangga Adithia <rangga.adithia26@gmail.com>
2023-04-15 10:24:36 +0800

    finish the matrix program

--- Matrix/main.c
+++ Matrix/main.c
@@ -4,11 +4,12 @@ int main()
 {
   int matrix1[2][3];
   int matrix2[3][2];
+  int result_matrix[2][2] = {{0, 0}, {0, 0}};
 
   printf("Masukan Matrix Pertama : \n");
   for (int i = 0; i < 2; i++)
   {
-    for (size_t j = 0; j < 3; j++)
+    for (int j = 0; j < 3; j++)
     {
       printf("Element [%d][%d] : ", i, j);
       scanf("%d", &matrix1[i][j]);
@@ -17,29 +18,30 @@ int main()
   printf("Masukan Matrix Kedua : \n");
   for (int i = 0; i < 3; i++)
   {
-    for (size_t j = 0; j < 2; j++)
+    for (int j = 0; j < 2; j++)
     {
       printf("Element [%d][%d] : ", i, j);
       scanf("%d", &matrix2[i][j]);
     }
   }
 
-  printf("Matrix Pertama: \n");
   for (int i = 0; i < 2; i++)
   {
-    for (size_t j = 0; j < 3; j++)
+    for (int j = 0; j < 2; j++)
     {
-      printf("%d ", matrix1[i][j]);
+      for (int k = 0; k < 3; k++)
+      {
+        result_matrix[i][j] += matrix1[i][k] * matrix2[k][j];
+      }
     }
-    printf("\n");
   }
 
-  printf("Matrix Kedua: \n");
+  printf("Matrix Result \n");
   for (int i = 0; i < 2; i++)
   {
-    for (size_t j = 0; j < 3; j++)
+    for (int j = 0; j < 2; j++)
     {
-      printf("%d ", matrix1[i][j]);
+      printf("%d ", result_matrix[i][j]);
     }
     printf("\n");
   }
---
+++ Matrix/tempCodeRunnerFile.c
@@ -0,0 +1,19 @@
+  // printf("Matrix Pertama: \n");
+  // for (int i = 0; i < 2; i++)
+  // {
+  //   for (size_t j = 0; j < 3; j++)
+  //   {
+  //     printf("%d ", matrix1[i][j]);
+  //   }
+  //   printf("\n");
+  // }
+
+  // printf("Matrix Kedua: \n");
+  // for (int i = 0; i < 3; i++)
+  // {
+  //   for (size_t j = 0; j < 2; j++)
+  //   {
+  //     printf("%d ", matrix1[i][j]);
+  //   }
+  //   printf("\n");
+  // }
