912cace9345ee2145f30164bb6c67a1d0c5ec0e7
Rangga Adithia <rangga.adithia26@gmail.com>
2023-06-09 20:24:38 +0800

    fix: parsing using delimiter function

--- Parsing Words/main.cpp
+++ Parsing Words/main.cpp
@@ -1,21 +1,42 @@
-#include "stdio.h"
-#include "string.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int delimit(char *kalimat, char *delimiter)
+{
+  int count = 0;
+  int length = strlen(kalimat);
+
+  for (int i = 0; i < length; i++)
+  {
+    if (!isalpha(kalimat[i]))
+    {
+      delimiter[count] = kalimat[i];
+      count++;
+    }
+  }
+  return count;
+}
 
 int main()
 {
   char kalimat[100];
+  char delimiter[100];
   char arrKata[100][100];
   int jumlahKata = 0;
-  printf("Masukkan kalimat: ");
-  fgets(kalimat, sizeof(kalimat), stdin);
 
-  char *kata = strtok(kalimat, " ,.");
-  printf("Kata-kata dalam kalimat:\n");
+  printf("Masukkan kalimat: ");
+  gets(kalimat);
+
+  delimit(kalimat, delimiter);
+
+  char *kata = strtok(kalimat, delimiter);
+
   while (kata != NULL)
   {
     strcpy(arrKata[jumlahKata], kata);
     jumlahKata++;
-    kata = strtok(NULL, " ,.\n");
+    kata = strtok(NULL, delimiter);
   }
 
   printf("Kata-kata dalam array:\n");
@@ -23,6 +44,5 @@ int main()
   {
     printf("%s\n", arrKata[i]);
   }
-
   return 0;
 }
